6587d0b12a1c00805967ea4e95774d90dfa90670
Mustafee <mussemoh03@gmail.com>
2025-03-12 21:05:26 +0100

    finished

--- src/components/sections/Faq.jsx
+++ src/components/sections/Faq.jsx
@@ -9,7 +9,8 @@ const Faq = () => {
         <div className="container">
             
             <div className="headline">
-                <h2>Any questions? Check out the FAQs</h2>
+                <h2>Any questions?</h2>
+                <h2>Check out the FAQs</h2>
                 <p>Still have unanswered questions and need to get in touch?</p>
             </div>
 
